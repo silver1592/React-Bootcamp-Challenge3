6c68435e2371a2ac57d60caf4eb633346632cdf7
Fernando Alvarez Flores <feral1592@gmail.com>
2021-06-23 23:55:41 -0500

    Display all pokemons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import CardItem from "./components/CardItem";
 import { pokemons } from "./mock/mockedData";
 
 import "./styles.css";
 
 export default function App() {
-  /**
-   * Input state management logic here
-   */
+  const [visiblePokemons, setVisiblePokemons] = useState(pokemons);
+
+  function OnChange(event) {}
 
   return (
     <div className="app">
       <h2>Mini Challenge 3: Poke filter</h2>
       {/**
        Insert here the input tag
-       **/}
+      **/}
+      <input type="text" placeholder="" name="input-search" />
+
       <div className="cards">
         {/**
         Filter pokemons data and map them to return an array of CardItems.
          */}
+        {visiblePokemons.map((poke) => (
+          <CardItem name={poke.name} sprite={poke.sprite} />
+        ))}
       </div>
     </div>
   );
